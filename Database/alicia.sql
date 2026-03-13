604a188deab7089d2abe071181710085145da706
Alyciabug <alicia.silva@sptech.school>
2026-03-13 00:22:58 +0000

    atualizei meu database

--- Database/alicia.sql
+++ Database/alicia.sql
@@ -10,14 +10,15 @@ CPF CHAR (12) UNIQUE,
 DTCadastro DATETIME DEFAULT CURRENT_TIMESTAMP
  ) AUTO_INCREMENT = 1000;
 
-INSERT INTO usuario values
-(DEFAULT, 'Roberto Carvalho', 'roberto.carvalho45@yahoo.com.br', '56731402R', 45671484556, default),
-(DEFAULT, 'Walquiria Novais', 'wal.novais@gmail.com', '28092007', 23457898745, default),
-(DEFAULT, 'Carlos Ohara', 'ohara.carlos@outlook.com.br', '23041967', '78965429867 ' , DEFAULT),
-(DEFAULT, 'Luiz cordeiro', 'lui.henrique.cordeiro@gmail.com', '30122009' ,'34565489067' , DEFAULT),
-(DEFAULT,  'Nicole Aparecida', 'Aparecida.nick@gmail.com', '11032008', '12343256789' , DEFAULT);
+CREATE TABLE sensores (
+id_sensor INT PRIMARY KEY AUTO_INCREMENT,
+id_empresa INT,
+locais CHAR (8),
+dados CHAR (4));
 
-SELECT * FROM usuario;
-
+CREATE TABLE calculo  (
+id_calc INT PRIMARY KEY AUTO_INCREMENT,
+dado_Temp CHAR (4),
+dado_Umidade CHAR (4));
 
  
